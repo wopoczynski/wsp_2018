85820cad85378bc1f17d908660a4011137fa08ad
Wojtek <wojtek.opoczynski@gmail.com>
2018-05-07 22:47:41 +0200

    working

--- expressionSet.R
+++ expressionSet.R
@@ -1,25 +1,24 @@
 source("http://bioconductor.org/biocLite.R")#ściąga skrypt instalacyjny
 
-setwd(paste(getwd(),"/data", sep=""))
-
-biocLite("BiocUpgrade")#uaktualnia, istnieje również mozliwość aktualizacji
+biocLite("biocUpgrade")
 biocLite("affy")
-biocLite("hgu95av2cdf") #working replacement for *.db lib
+biocLite("gahgu95av2.db")
 biocLite("gplots")
-biocLite("convert")
-biocLite("Biobase")
+biocLite("gahgu95av2cdf")
 
 library("Biobase")
 library("affy")
-library("hgu95av2cdf")
-library('gahgu95av2cdf')
-library("gplots") #heatmap.2
-library("convert")
+library("gahgu95av2.db")
+library("gplots")
+library("gahgu95av2cdf")
+
 
 # clear env
 rm(list = ls())
 
 #tworzenie pełnego objektu ExpressionSet(zgodnie z instrukcją z pdf)
+
+setwd(paste(getwd(),"/data", sep=""))
 
 data <- read.table("datasetA_scans.txt", header = T, sep = "\t")
 description <- read.AnnotatedDataFrame("datasetA_scans.txt", header = T, sep = "\t", 
@@ -29,11 +28,7 @@ dataAffy = ReadAffy(verbose=TRUE, filenames=sampleNames(description))
 dataAffy@cdfName = paste("ga", dataAffy@cdfName, sep = "")
 dataAffy@annotation = paste("ga", dataAffy@annotation, sep = "")
 
-# nie działa funkcja rma znaleźć alternatywe bo bez tego dalej nie poleci
-normRMA = expresso(dataAffy, normalize.method = 'rma',
-                   normalize.method="constant",pmcorrect.method="pmonly",
-                   summary.method="avgdiff")
-
+normRMA = rma(dataAffy)
 dataRMA = exprs(normRMA)
 
 
@@ -48,4 +43,5 @@ featN = dim(ExprSet)[1]
 cutoff = round(featN*0.025)
 indClear = exprSort$ix[c(1:cutoff, (featN - cutoff):featN)]
 
-ExprSet = ExprSet[-indClear,]
+ExprSet = ExprSet[-indClear, NULL]
+
