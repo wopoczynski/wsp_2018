73f8697f3e5ba98c090e913704117b5da4983935
Wojtek <wojtek.opoczynski@gmail.com>
2018-05-21 12:11:10 +0200

    wydzielenie potrzebnych bibliotek do innego pliku

--- expressionSet.R
+++ expressionSet.R
@@ -1,17 +1,4 @@
-source("http://bioconductor.org/biocLite.R")#ściąga skrypt instalacyjny
-
-biocLite("biocUpgrade")
-biocLite("affy")
-biocLite("gahgu95av2.db")
-biocLite("gplots")
-biocLite("gahgu95av2cdf")
-
-library("Biobase")
-library("affy")
-library("gahgu95av2.db")
-library("gplots")
-library("gahgu95av2cdf")
-
+source(paste(getwd(),"/libs.R", sep=""))
 
 # clear env
 rm(list = ls())
