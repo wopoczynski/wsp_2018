2679d076c7d16b72a158099cfece90d8dde40729
Wojtek <wojtek.opoczynski@gmail.com>
2018-06-11 14:59:02 +0200

    libs

--- R/libs.R
+++ R/libs.R
@@ -1,14 +1,12 @@
-# required libs
-source("http://bioconductor.org/biocLite.R")#ściąga skrypt instalacyjny
-
-source("https://bioconductor.org/biocLite.R")
-biocLite()
-
-source(paste(getwd(),"/packages", sep=""))
-
-install.packages(paste(getwd(),"/packages/gahgu95av2.db_2.2.0.tar.gz", repos = NULL, type="source"))
-install.packages(paste(getwd(),"/packages/gahgu95av2cdf_2.2.1.tar.gz", repos = NULL, type="source"))
-
+# required libs przy pierwszym odpalaniu odkomentowac
+# source("http://bioconductor.org/biocLite.R")#ściąga skrypt instalacyjny
+# biocLite()
+# biocLite("biocUpgrade")
+# biocLite("affy")
+# biocLite("gahgu95av2.db")
+# biocLite("gplots")
+# install.packages(paste(getwd(),"/packages/gahgu95av2.db_2.2.0.tar.gz"), repos = NULL, type="source")
+# install.packages(paste(getwd(),"/packages/gahgu95av2cdf_2.2.1.tar.gz"), repos = NULL, type="source")
 
 library("Biobase")
 library('affy')
