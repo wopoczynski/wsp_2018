6b1a8b27f73355777ad86b36c5c2b3b557050003
Karol <k.mandel@wp.pl>
2018-06-11 14:31:33 +0200

    path

--- data/heatmaps.R
+++
@@ -1,8 +0,0 @@
-#argumentem do funkcji jest zmienna typu "data" powstała po użyciu funkji exprs na danych po normalizacji:
-#normRMA = rma(dataAffy)
-#dataRMA = exprs(normRMA)
-
-#przykładowe użyciena podstawie zmiennych z expressionSet.R: heatmaps(dataRMA)
-heatmaps <- function(heatmap_data){
-  heatmap(heatmap_data, Colv=NA, Rowv=NA, scale="column", cexRow = 0.1, cexCol = 1)
-}
