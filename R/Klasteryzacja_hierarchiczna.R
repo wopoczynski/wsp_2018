029485444a1f10b6bc19d13e59361e0da7ca92df
Basia <basia.bz@op.pl>
2018-07-02 12:40:27 +0200

    Klasteryzacja_hierarchiczna

--- R/Klasteryzacja_hierarchiczna.R
+++ R/Klasteryzacja_hierarchiczna.R
@@ -1,5 +1,5 @@
 Klasteryzacja_hierarchiczna = function(ExprSet,a,b,c,d) {
   Klasteryzacja_h =exprs(ExprSet)
   clusters = hclust(dist(Klasteryzacja_h[a:b,c:d]))
-  plot(clusters)
+  plot(clusters,xlab="Clusters", sub="")
 }
