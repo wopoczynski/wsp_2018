98d3996976d8db62c5632f5ecae641a8299ef9c9
Basia <basia.bz@op.pl>
2018-07-02 12:29:49 +0200

    PCA

--- R/PCA.R
+++ R/PCA.R
@@ -2,7 +2,8 @@ PCA = function(ExprSet) {
   PCA_model = prcomp(t(exprs(ExprSet)))
   adeno = which(pData(ExprSet)$CLASS == 'ADENO')
   squamous = which(pData(ExprSet)$CLASS == 'SQUAMOUS')
-  colors = ifelse(pData(ExprSet)$CLASS == 'ADENO','red','blue')
+  colors = ifelse(pData(ExprSet)$CLASS == 'ADENO','2','3')
   plot(PCA_model$x[,1:2],col = colors,main = 'PCA')
+  legend("topright",c("Adeno", "Squamous"),col = c(2, 3), pch = c(1, 1))
   barplot(PCA_model$sdev[1:5] / sum(PCA_model$sde), main = 'PCA')
 }
